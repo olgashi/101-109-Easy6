a6a777518d0f21f064877cde246b827b8a55c366
Oshi99 <oshiryaeva@protonmail.com>
2018-08-13 23:44:14 -0500

    refactored, still needs work

--- delete_vowels.rb
+++ delete_vowels.rb
@@ -1,14 +1,18 @@
 # Fix
+# returns no empty array if all letters in element removed
 
 VOWELS = %w( a e i o u )
-
-def remove_vowels(strings)
-  arr_modified = strings.each do |item| 
-    item.split('').map do |letter| 
-      item.delete!(letter) if VOWELS.include?(letter)
-    end
-  end
-end
 str = %w(green YELLOW black white)
-result = %w(grn YLLW blck wht)
-puts remove_vowels(str) == result
+
+# def remove_vowels(strings)
+#   arr_modified = strings.each do |item| 
+#     item.split('').map do |letter| 
+#       item.delete!(letter) if VOWELS.include?(letter)
+#     end
+#   end
+# end
+def remove_vowels(str)
+  str.join(' ').chars.keep_if { |l| !VOWELS.include?(l.downcase) }.join('').split(' ')
+end
+
+p remove_vowels(%w(ABC AEIOU XYZ))
