3b6e78d6231d100fc964e182ee1ccb6adff09b95
Oshi99 <oshiryaeva@protonmail.com>
2018-08-14 20:16:44 -0500

    minor fixes

--- reversed_arrays1.rb
+++ reversed_arrays1.rb
@@ -1,10 +1,11 @@
-arr = [1, 2, 3, 4, 5, 6]
-i = 0
-while i < arr.size/2
-  temp = arr[i]
-  arr[i] = arr[(-1) - i]
-  arr[(-1) - i] = temp
-  i += 1
-end
+def reverse(arr)
+  i = 0
+  while i < arr.size/2
+    temp = arr[i]
+    arr[i] = arr[(-1) - i]
+    arr[(-1) - i] = temp
+    i += 1
+  end
 
-p arr
+  arr
+end
